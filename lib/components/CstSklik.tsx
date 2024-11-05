1b395bca1eb4075904ba1eebf06143efb5a5cd2a
thangnd.omegatheme <thangnd@omegatheme.com>
2024-11-05 10:13:00 +0700

    remove attribute script

--- lib/components/CstSklik.tsx
+++ lib/components/CstSklik.tsx
@@ -17,7 +17,7 @@ const CstSKlik: React.FC<{nonce?: string; sklikId?: string}> = ({
         src="https://c.seznam.cz/js/rc.js"
       />
       <script
-        strategy="beforeInteractive"
+        // strategy="beforeInteractive"
         nonce={nonce}
         dangerouslySetInnerHTML={{
           __html: `var retargetingConf={rtgId:${sklikId},consent:0};function _otkSklikConsent(t=0){window.sznIVA?.IS.updateIdentities({eid:null});var n,e;const i=(n="cookiesNotification",e=["marketing","analytics"],document.cookie.split(";").some((t=>(t=t.trim()).startsWith(n+"=")&&e.some((n=>t.includes(n))))))?1:t;retargetingConf={...retargetingConf,consent:i},window.rc?.retargetingHit(retargetingConf)}_otkSklikConsent();`,
