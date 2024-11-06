450bdb1d0216c5d3d08ebb91dc7381e332c88ec7
thangnd.omegatheme <thangnd@omegatheme.com>
2024-11-06 08:58:45 +0700

    hide script header

--- lib/components/CstScript.tsx
+++ lib/components/CstScript.tsx
@@ -1,37 +1,27 @@
 import * as React from 'react';
 import {Fragment} from 'react';
 import {IMetaField} from '../utils/types.ts';
-import CstGCM from './CstGCM.tsx';
-import RenderIf from './RenderIf.tsx'
+import RenderIf from './RenderIf.tsx';
+
 interface IProps {
   nonce?: string;
   banner: IMetaField | undefined | null;
 }
 
-const CstScript: React.FC<IProps> = ({nonce, banner}): React.JSX.Element => {
-    return (
+const CstScript: React.FC<IProps> = ({banner}): React.JSX.Element => {
+  return (
     <Fragment>
-     <RenderIf cond={!!banner}>
-         <style>{banner?.setting?.custom_css || ''}</style>
-         {/*<CstAutoBlock*/}
-         {/*  option={banner.integration.autoBlock}*/}
-         {/*  nonce={nonce}*/}
-         {/*/>*/}
-         <CstGCM {...banner} nonce={nonce} />
-         {/*<CstSklik sklikId={banner.integration?.sklik} nonce={nonce} />*/}
-         {/*<CstUET UET={banner.integration?.uet} nonce={nonce} />*/}
-         <script
-             nonce={nonce}
-             dangerouslySetInnerHTML={{
-                 __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-               })(window,document,'script','dataLayer','GTM-T535TVSJ');`,
-             }}
-         />
-     </RenderIf>
+      <RenderIf cond={!!banner}>
+        <style>{banner?.setting?.custom_css || ''}</style>
+        {/*<CstAutoBlock*/}
+        {/*  option={banner.integration.autoBlock}*/}
+        {/*  nonce={nonce}*/}
+        {/*/>*/}
+        {/*<CstGCM {...banner} nonce={nonce} />*/}
+        {/*<CstSklik sklikId={banner.integration?.sklik} nonce={nonce} />*/}
+        {/*<CstUET UET={banner.integration?.uet} nonce={nonce} />*/}
+      </RenderIf>
     </Fragment>
   );
 };
-export default CstScript;
+export default CstScript
